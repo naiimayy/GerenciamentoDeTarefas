6c6c98fbc69d8ad7aa3ab32c9918f667d27a28a1
naiimayy <naiiaramay@gmail.com>
2019-07-15 20:44:55 -0300

    Criado CategoriaRepository. Criado CategoriaController.

--- Model/Categoria.cs
+++ Model/Categoria.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Categoria
+    [Table("categorias")]
+    public class Categoria: Base
     {
+        [Column("id")]
+        public int Id { get; set }
 
+        [Column("nome")]
+        public string Nome { get; set; }
     }
 }
---
+++ View/Controllers/CategoriaController.cs
@@ -0,0 +1,63 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class CategoriaController: Controller
+    {
+        private CategoriaRepository repository;
+
+        //GET: Categgoria
+        public CategoriaController()
+        {
+            repository = new CategoriaRepository();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ObterTodos(string busca)
+        {
+            List<Categoria> categorias = repository.ObterTodos(busca);
+            return Json(categorias, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Store(Categoria categoria)
+        {
+            categoria.RegistroAtivo = true;
+            repository.Inserir(categoria);
+            return Json(categoria);
+        }
+
+        [HttpGet]
+        [Route ("apagar/{id}")]
+        public JsonResult Apagar(int id)
+        {
+            bool apagou = repository.Apagar(id);
+            return Json(new { status = apagou }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet, Route("ObterPeloId/{id}")]
+        public JsonResult ObterPeloId(int id)
+        {
+            Categoria categoria = repository.ObterPeloId(id);
+            return Json(categoria, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Update(Categoria categoria)
+        {
+            bool alterou = repository.Alterar(categoria);
+            return Json(new { status = alterou });
+        }
+    }
+}
