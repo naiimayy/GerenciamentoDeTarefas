77bca07ef3d04b568143dedc59953305dc21f39c
Guilherme <48655138+guilhermeLRibeiroo@users.noreply.github.com>
2019-07-15 18:07:51 -0300

    Adicionado .gitignore, Model Base.cs e Adicionado Database.mdf no AppData da View.

---
+++ Model/Base.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Base
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("data_criacao")]
+        public DateTime DataCriacao { get; set; }
+
+        [Column("registro_ativo")]
+        public bool RegistroAtivo { get; set; }
+
+    }
+}
