d3b6124be4beedd907a39c59dbdc682251458f01
Guilherme <48655138+guilhermeLRibeiroo@users.noreply.github.com>
2019-07-15 18:14:42 -0300

    Adicionado SistemaContext no Repository.Database

---
+++ Repository/DataBase/SistemaContext.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DataBase
+{
+    class SistemaContext : DbContext
+    {
+        public SistemaContext() : base("DefaultConnection")
+        {       
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
