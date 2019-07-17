bb5a11402a166915862ebf3545a0edb62545023c
naiimayy <naiiaramay@gmail.com>
2019-07-17 18:54:29 -0300

    Criado ICategoriaCidade

--- Repository/Interfaces/ICidadeRepository.cs
+++ Repository/Interfaces/ICidadeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace Repository.Interfaces
 {
     interface ICidadeRepository
     {
+        int Inserir(Cidade cidade);
+
+        bool Atualizar(Cidade cidade);
+
+        bool Apagar(int id);
+
+        Cidade ObterPeloId(int id);
+
+        List<Cidade> ObterTodos(string busca);
     }
 }
