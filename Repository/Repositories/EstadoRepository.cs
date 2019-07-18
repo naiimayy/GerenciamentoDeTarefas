793c799c77e8daf9f3693266814fca081b27b9a1
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-07-17 21:12:58 -0300

    Update EstadoRepository.cs

--- Repository/Repositories/EstadoRepository.cs
+++ Repository/Repositories/EstadoRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Repository.DataBase;
+using Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Repository.Repositories
 {
-    class EstadoRepository
+    public class EstadoRepository : IEstadoRepository
     {
+        SistemaContext context;
+
+        public EstadoRepository()
+        {
+            context = new SistemaContext();
+        }
+
     }
 }
