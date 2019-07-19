3656fb854b8ffba3f222757daaa707e03e6a0bd3
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-07-19 19:08:03 -0300

    Feito controller Estado

---
+++ View/Controllers/EstadoController.cs
@@ -0,0 +1,64 @@
+﻿using Model;
+using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class EstadoController : Controller
+    {
+        EstadoRepository repository;
+
+        public EstadoController()
+        {
+            repository = new EstadoRepository();
+        }
+        // GET: Estado
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ObterTodos()
+        {
+            List<Estado> estados = repository.ObterTodos("");
+            return Json(estados, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Store(Estado estado)
+        {
+            estado.RegistroAtivo = true;
+            repository.Inserir(estado);
+            return Json(estado);
+        }
+
+        [HttpGet, Route("apagar/{id}")]
+        public JsonResult Apagar(int id)
+        {
+            bool apagou = repository.Apagar(id);
+            return Json(new { status = apagou }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet, Route("obterpeloId/{id")]
+        public JsonResult ObterPeloId(int id)
+        {
+            Estado estado = repository.ObterPeloId(id);
+            return Json(estado, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Update(Estado estado)
+        {
+            bool alterou = repository.Atualizar(estado);
+            return Json(new { status = alterou });
+        }
+
+
+    }
+}
