1b9108986167d29b02cc4cdebcee8a9a99174507
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-07-19 17:40:41 -0300

    Feito apagar estado.

--- Repository/Repositories/EstadoRepository.cs
+++ Repository/Repositories/EstadoRepository.cs
@@ -1,4 +1,5 @@
-﻿using Repository.DataBase;
+﻿using Model;
+using Repository.DataBase;
 using Repository.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,39 @@ namespace Repository.Repositories
         {
             context = new SistemaContext();
         }
+
+        public int Inserir(Estado estado)
+        {
+            return 2;
+        }
+
+        public bool Atualizar(Estado estado)
+        {
+            return Estado;
+        }
+
+        public bool Apagar(int id)
+        {
+            Estado estado = (from x in context.Estados where x.Id == id select x).FirstOrDefault();
+            if (estado == null)
+                return false;
+
+            estado.RegistroAtivo = false;
+            context.SaveChanges();
+            return true;
+        }
+
+
+        public Estado ObterPeloId(int id)
+        {
+
+        }
+
+        public List<Estado> ObterTodos(string busca)
+        {
+
+        }
+
 
     }
 }
