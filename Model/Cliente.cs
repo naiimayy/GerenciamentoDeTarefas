7c35770d288dd2a0b60d23d51e8f46034952b0ae
naiimayy <naiiaramay@gmail.com>
2019-07-22 19:29:44 -0300

    Criado Model Cliente

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Cliente
+    [Table("clientes")]
+    public class Cliente: Base
     {
+        [Column("id_cliente")]
+        public int IdCliente { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("cpf")]
+        public int Cpf { get; set; }
+
+        [Column("data_nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [Column("numero")]
+        public int Numero { get; set; }
+
+        [Column("complemento")]
+        public string Complemento { get; set; }
+
+        [Column("logradouro")]
+        public string Logradouro { get; set; }
+
+        [Column("cep")]
+        public int Cep { get; set; }
     }
 }
