0c8e74e6d1c110c130ca1a718913c7cc4ed32c40
Guilherme <48655138+guilhermeLRibeiroo@users.noreply.github.com>
2019-07-27 19:09:43 -0300

    Adicionado TarefaController, Script tarefa cadastro.js e View index.cshtml e corrigido bug na view usuario

---
+++ View/Controllers/TarefaController.cs
@@ -0,0 +1,26 @@
+﻿using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class TarefaController : Controller
+    {
+        TarefaRepository repository;
+
+        public TarefaController()
+        {
+            repository = new TarefaRepository();
+        }
+
+        // GET: Tarefa
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
