a150db4896fd00313ff6486525a1ec55ba73b80c
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-07-28 00:22:55 -0300

    Feito view Projeto e Ajax

--- View/Controllers/ProjetoController.cs
+++ View/Controllers/ProjetoController.cs
@@ -11,56 +11,55 @@ namespace View.Controllers
     public class ProjetoController : Controller
     {
         ProjetoRepository repository;
-      
-            public ProjetoController()
-            {
-                repository = new ProjetoRepository();
-            }
-            // GET: Projeto
-            [HttpGet]
+
+        public ProjetoController()
+        {
+            repository = new ProjetoRepository();
+        }
+        // GET: Projeto
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-            [HttpGet]
-            public JsonResult ObterTodos()
-            {
-                List<Projeto> projetos = repository.ObterTodos("");
-                return Json(projetos, JsonRequestBehavior.AllowGet);
-            }
-
-            [HttpPost]
-            public JsonResult Store(Projeto projeto)
-            {
-                projeto.RegistroAtivo = true;
-                repository.Inserir(projeto);
-                return Json(projeto);
-            }
-
-            [HttpGet, Route("apagar/{id}")]
-            public JsonResult Apagar(int id)
-            {
-                bool apagou = repository.Apagar(id);
-                return Json(new { status = apagou }, JsonRequestBehavior.AllowGet);
-            }
-
-            [HttpGet, Route("obterpeloId/{id")]
-            public JsonResult ObterPeloId(int id)
-            {
-               Projeto projeto = repository.ObterPeloId(id);
-                return Json(projeto, JsonRequestBehavior.AllowGet);
-            }
-
-            [HttpPost]
-            public JsonResult Update(Projeto projeto)
-            {
-                bool alterou = repository.Atualizar(projeto);
-                return Json(new { status = alterou });
-            }
-
-
+        [HttpGet]
+        public JsonResult ObterTodos()
+        {
+            List<Projeto> projetos = repository.ObterTodos("");
+            return Json(projetos, JsonRequestBehavior.AllowGet);
         }
-    }
 
+        [HttpPost]
+        public JsonResult Store(Projeto projeto)
+        {
+            projeto.RegistroAtivo = true;
+            repository.Inserir(projeto);
+            return Json(projeto);
+        }
+
+        [HttpGet, Route("apagar/{id}")]
+        public JsonResult Apagar(int id)
+        {
+            bool apagou = repository.Apagar(id);
+            return Json(new { status = apagou }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet, Route("obterpeloId/{id")]
+        public JsonResult ObterPeloId(int id)
+        {
+            Projeto projeto = repository.ObterPeloId(id);
+            return Json(projeto, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Update(Projeto projeto)
+        {
+            bool alterou = repository.Atualizar(projeto);
+            return Json(new { status = alterou });
+        }
+
+
+    }
 }
+
